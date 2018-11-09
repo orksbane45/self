47711a277fda71e0fc96adb7ca86c1bf803910b8
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-11-09 19:51:12 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,6 +5,6 @@ module.exports = client => {
     console.log(chalk.inverse(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] `) + chalk.black.bgGreen(`Le self-bot ${client.user.tag} est prêt.`));
     console.log(chalk.inverse('Token :               ') + chalk.black.bgGreen(client.token));
     console.log(chalk.inverse('Id :                  ') + chalk.black.bgGreen(client.user.id));
-  client.user.setActivity('être un chevalier')
+  client.user.setActivity('être un chevalier').timestamps()
 };
  //on ou off
