34a5a8bd9c946dbf6924c3d64ce32c05d4f95eb5
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2019-01-26 15:39:06 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ const moment = require('moment');
 module.exports = client => {
  console.log(chalk.black.bgRed('Self Bot en ligne'));
     console.log(chalk.inverse(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] `) + chalk.black.bgGreen(`Le self-bot ${client.user.tag} est prêt.`));
-  client.user.setActivity('être un chevalier')
+  client.user.setActivity('rien')
 };
  //on ou off
